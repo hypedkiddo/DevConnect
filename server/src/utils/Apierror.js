193d3def357b4b50febdc8cc9838fde15c4f7cbd
crazypunk08 <naren.lakamannavar@gmail.com>
2025-07-10 13:01:39 +0530

    Refactor database connection to use environment variable for DATABASE_URL; add ApiError, ApiResponse, and asyncHandler utility classes

---
+++ server/src/utils/Apierror.js
@@ -0,0 +1,18 @@
+class Apierror extends Error{
+    constructor(statuscode,message="Soemthing went wrong",error=[],stack=""){
+        super(message);
+        this.statuscode=statuscode;
+        this.data=null;
+        this.message=message;
+        this.success=false;
+        this.errors=this.errors
+        if(stack){
+            this.stack=stack
+        }
+        else{
+            Error.captureStackTrace(this,this.constuctor)
+        }
+    }
+}
+
+export default Apierror
