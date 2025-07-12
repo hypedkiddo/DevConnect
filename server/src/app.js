32a8f75647cd8e88fadc4ecc69bb6dc597c02c47
crazypunk08 <naren.lakamannavar@gmail.com>
2025-07-12 12:49:10 +0530

    Add user routes for registration, login, and logout functionality

--- server/src/app.js
+++ server/src/app.js
@@ -14,6 +14,14 @@ app.use(urlencoded({extended:true,limit:"16kb"}));
  app.use(express.static("public"));
  app.use(cookieParser());
 
+ //User
+ import UserRouter from "./routes/user.routes.js"
+ app.use("/api/v1/users",UserRouter);
+
+
+
+
+ //Home route
  app.get("/",(req,res)=>{
     res.send("Welcome to my Project");
  })
---
+++ server/src/routes/user.routes.js
@@ -0,0 +1,17 @@
+import { Router } from "express"
+import {upload} from "../middlewares/multer.middleware.js"
+import {registerUser,loginUser,logoutuser} from "../controllers/user.controller.js"
+
+const router=Router();
+//Register route
+router.route("./register").post(upload.fields([
+    {
+        name:"avatar",
+        maxCount:1
+    }
+]),registerUser);
+//Login route
+router.route("./login").post(loginUser);
+//Logout user
+router.route("./logout").post(loginUser);
+export default router;
